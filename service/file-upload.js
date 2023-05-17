ab4003b44754d6de5a6aaa9d64d7cc3ebf1fcca6
Gosia Antoniak/gosia-antoniak_89@o2.pl <gosia-antoniak_89@o2.pl>
2023-05-17 15:03:28 +0200

    final commit

--- service/file-upload.js
+++ service/file-upload.js
@@ -1,13 +1,13 @@
-const Jimp = require('jimp');
-const fs = require('fs/promises');
-const path = require('path');
+const Jimp = require('jimp')
+const fs = require('fs/promises')
+const path = require('path')
 class UploadFileAvatar {
     constructor(destination) {
-        this.destination = destination;
+        this.destination = destination
     }
 
     async transformAvatar(pathFile) {
-        const pic = Jimp.read(pathFile);
+        const pic = Jimp.read(pathFile)
         await (
             await pic
         )
@@ -17,13 +17,13 @@ class UploadFileAvatar {
                 250,
                 Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE,
             )
-            .writeAsync(pathFile);
+            .writeAsync(pathFile)
     }
 
     async save(file, idUser) {
-        await this.transformAvatar(file.path);
-        await fs.rename(file.path, path.join(this.destination, file.filename));
-        return path.normalize(path.join(idUser, file.filename));
+        await this.transformAvatar(file.path)
+        await fs.rename(file.path, path.join(this.destination, file.filename))
+        return path.normalize(path.join(idUser, file.filename))
     }
 }
 
